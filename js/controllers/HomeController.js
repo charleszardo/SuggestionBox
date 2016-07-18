c6d400795eaf6bd5fce455e4ae4ec95b4e70b95f
Dave Charnuska <davecharnuska@gmail.com>
2016-07-18 13:13:45 -0400

    add form and addSuggestion()

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,17 @@
 app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
-	$scope.posts = suggestions.posts
+	$scope.posts = suggestions.posts;
+	
+	$scope.addSuggestion = function() {
+		if(!$scope.title || $scope.title === '') {
+			return;
+		}
+		
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes: 0,
+			comments: []
+		});
+		
+		$scope.title = '';
+	};
 }]);
