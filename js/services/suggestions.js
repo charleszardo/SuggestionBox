ac62e60276206c78a8109f202939056fe273107a
Dave Charnuska <davecharnuska@gmail.com>
2016-07-18 13:04:52 -0400

    add demo suggestions

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -2,10 +2,35 @@ app.factory('suggestions', [function() {
 	var demoSuggestions = {
 		posts: [
 			{
-				title: '',
+				title: 'Suggestion 1',
 				upvotes: 10,
-				comments[]
-			}
+				comments: []
+			},
+			{
+				title: 'Suggestion 2',
+				upvotes: 2,
+				comments: []
+			},
+			{
+				title: 'Suggestion 3',
+				upvotes: 27,
+				comments: []
+			},
+			{
+				title: 'Suggestion 4',
+				upvotes: 19,
+				comments: []
+			},
+			{
+				title: 'Suggestion 5',
+				upvotes: 1,
+				comments: []
+			},
+			{
+				title: 'Suggestion 6',
+				upvotes: 8,
+				comments: []
+			},
 		]
 	}
 	
