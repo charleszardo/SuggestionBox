81564078b933b108e268c1f5504939471a134d89
Dave Charnuska <davecharnuska@gmail.com>
2016-07-20 14:43:13 -0400

    add gulp-sass, begin writing sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-var gulp = require('gulp')
-var browserSync = require('browser-sync').create()
-var reload = browserSync.reload
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('serve', () => {
   browserSync.init({
     server: './'
   })
-	gulp.watch('index.html').on('change', reload)
-	gulp.watch('views/*.html').on('change', reload)
-  gulp.watch('css/*.css').on('change', reload)
-	gulp.watch('js/**/*.js').on('change', reload)
+	gulp.watch('index.html').on('change', reload);
+	gulp.watch('views/*.html').on('change', reload);
+  gulp.watch('css/*.css').on('change', reload);
+	gulp.watch('js/**/*.js').on('change', reload);
+	gulp.watch('sass/**/*.scss',['styles']);
 })
+
+gulp.task('styles', function() {
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css/'));
+});
