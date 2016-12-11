7cbb9a50827cedac70dcb13a5b4b489d7cf1ecd1
Dave Charnuska <davecharnuska@gmail.com>
2016-12-11 18:26:04 -0500

    add validations and helper methods to User model

--- suggestion_box/app/models/user.rb
+++ suggestion_box/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
