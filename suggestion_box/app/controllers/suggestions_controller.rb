c335fe298cb155f8545127356e40d554e053c9cf
Dave Charnuska <davecharnuska@gmail.com>
2016-12-12 17:10:10 -0500

    add new/edit forms, make relevant controller changes

--- suggestion_box/app/controllers/suggestions_controller.rb
+++ suggestion_box/app/controllers/suggestions_controller.rb
@@ -10,9 +10,9 @@ class SuggestionsController < ApplicationController
     @suggestion.author = current_user
 
     if @suggestion.save
-      redirect_to :index
+      redirect_to suggestion_url(@suggestion)
     else
-      render @suggestion.errors.full_messages
+      render json: @suggestion.errors.full_messages
     end
   end
 
@@ -20,6 +20,16 @@ class SuggestionsController < ApplicationController
     @suggestion = Suggestion.new
 
     render :new
+  end
+
+  def edit
+    @suggestion = Suggestion.find_by(params[:id])
+
+    if @suggestion.save
+      redirect_to suggestion_url(@suggestion)
+    else
+      render json: @suggestion.errors.full_messages
+    end
   end
 
   private
