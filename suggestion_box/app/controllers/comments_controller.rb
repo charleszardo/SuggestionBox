ad20453c4336be64c1af9009c9d8d523e171a9b1
Dave Charnuska <davecharnuska@gmail.com>
2016-12-13 01:40:00 -0500

    add ability to post and display comments

--- suggestion_box/app/controllers/comments_controller.rb
+++ suggestion_box/app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    fail
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    
+    if @comment.save
+      redirect_to suggestion_url(@comment.suggestion)
+    else
+      render json: @comment.errors.full_messages
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :suggestion_id)
   end
 end
