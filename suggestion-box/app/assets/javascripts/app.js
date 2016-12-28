1c6279ffc19e81314a77e27a4325465138fef27e
Dave Charnuska <davecharnuska@gmail.com>
2016-12-28 01:01:03 -0500

    move app.js to rails javascripts folder

---
+++ suggestion-box/app/assets/javascripts/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('SuggestionBox', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/home.html',
+		controller: 'HomeController'
+	})
+	.when('/suggestion/:id', {
+		templateUrl: 'views/suggestion.html',
+		controller: 'SuggestionController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
+});
