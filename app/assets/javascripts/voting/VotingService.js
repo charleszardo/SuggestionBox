79b2a4d24b4f49d87d420da7720d57a473c7b42e
Dave Charnuska <davecharnuska@gmail.com>
2016-12-30 12:03:21 -0500

    change voting service to service rather than factory

--- app/assets/javascripts/voting/VotingService.js
+++ app/assets/javascripts/voting/VotingService.js
@@ -1,15 +1,11 @@
-app.factory('VotingService', ['$http', function($http) {
-	var o = {};
-
-	o.vote = function(obj, val) {
+app.service('VotingService', ['$http', function($http) {
+	this.vote = function(obj, val) {
 		// TODO improve way of determing if obj is suggestion or comment
 		var resource = obj.title ? '/suggestions/' : '/comments/',
-				params = { upvote: val}
-				
+				params = { upvote: val};
+
 		return $http.post(resource + obj.id + '/votes.json', params).then(function(success) {
 			obj.vote_count += (val === true ? 1 : -1)
-		})
-	}
-
-	return o;
+		});
+	};
 }]);
